ecf441f91c87a59f7cde0146a07ca236a85abea3
Aakash <aakash.yogabalu@gmail.com>
2025-03-13 22:31:44 -0300

    Added routing for all the pages available

--- game-management-system/src/App.jsx
+++ game-management-system/src/App.jsx
@@ -1,6 +1,21 @@
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Favorites from "./pages/Favorites"
+import Login from "./pages/Login"
+import Cart from "./pages/Cart"
+import Profile from "./pages/Profile"
+
+
 function App() {
-    
+  return (
+      <Routes>
+        <Route path= "/" element={<Home />}/>
+        <Route path= "/favorites" element={<Favorites />}/>
+        <Route path= "/login" element={<Login />}/>
+        <Route path= "/cart" element={<Cart />}/>
+        <Route path= "/profile" element={<Profile />}/>
+      </Routes>
+  )
 }
-  
-  export default App
-  
+
+export default App
