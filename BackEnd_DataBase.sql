87cc943fd3d321cf80402b6159a4a27b7e8bd29e
gavin <gavinhosford@gmail.com>
2025-03-14 15:02:04 -0300

    added Inventory and alter the pre-existing tables.

--- BackEnd_DataBase.sql
+++ BackEnd_DataBase.sql
@@ -2,26 +2,40 @@ CREATE DATABASE game_ziad_store;
 USE game_ziad_store;
 
 
+CREATE TABLE inventory(
+	inventory_id INT PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+	price DECIMAL(10,2),
+	discount INT,--this would be the percent out of 100 that would be the discount
+    quantity_in_stock INT DEFAULT 0,
+    description TEXT,
+	warranty DATE
+);
 CREATE TABLE games (
     game_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
     category VARCHAR(100),
     console VARCHAR(50),
-    price DECIMAL(10,2),
-    quantity_in_stock INT DEFAULT 0,
-    release_date DATE,
-    publisher VARCHAR(100),
-    esrb_rating VARCHAR(10),
-    description TEXT,
-    image_path VARCHAR(255)
+	inventory_id int,
+	FOREIGN KEY (inventory_id) REFERENCES inventory(inventory_id) ON DELETE SET NULL
 );
+CREATE TABLE merch (
+	merch_id INT PRIMARY KEY AUTO_INCREMENT,
+	type enum ('Hat','T-Shirt','Hoodie','Bottle','Socks'),
+	size enum ('XS','S','M','L','XL'),
+	inventory_id int,
+	FOREIGN KEY (inventory_id) REFERENCES inventory(inventory_id) ON DELETE SET NULL
+);
+
+
+
+
+
 
 CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 --name is their actual name and not user name
 CREATE TABLE managers (
@@ -29,39 +43,19 @@ CREATE TABLE managers (
     user_id INT UNIQUE,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE,
-    salary DECIMAL(10,2),
-    hire_date DATE,
-    department VARCHAR(100),
-    access_level INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE SET NULL
 );
---wage is per hour
---left the schedule blank as i dont really know how it should be altered
+
 CREATE TABLE employees (
     employee_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT UNIQUE,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE,
-    wage DECIMAL(10,2),
-    schedule TEXT,
-    hire_date DATE,
-    department VARCHAR(100),
-	access_level INT,
     manager_id INT,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (manager_id) REFERENCES managers(manager_id) ON DELETE SET NULL
 );
 
-CREATE TABLE stock_updates (
-    update_id INT PRIMARY KEY AUTO_INCREMENT,
-    game_id INT,
-    updated_by_id INT,
-    updated_by_type ENUM('employee', 'manager'),
-    old_quantity INT,
-    new_quantity INT,
-    update_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (game_id) REFERENCES games(game_id)
-);
 --this info is just for show
 INSERT INTO USER(username, password, email)
 values 
@@ -75,17 +69,25 @@ values
 	select * from user;
 	
 	
-	INSERT INTO employees(user_id, name, wage, hire_date, department, access_level, manager_id)
+	INSERT INTO employees(user_id, name, access_level, manager_id)
 	values
-	('1', 'Clark Kent', '17.80', NOW(), 'Cashier', '1', '1'), 
-	('2', 'Diana Prince', '17.85', NOW(), 'Stocker', '1',  '1'),
-	('4', 'Hal Jordan', '19.90', NOW(), 'Supervisor', '2',  '1');
+	('1', 'Clark Kent', '1', '1'), 
+	('2', 'Diana Prince', '1',  '1'),
+	('4', 'Hal Jordan', '2',  '1');
 	
 	select * from employees;
 	
-	INSERT INTO managers (user_id, name, salary, hire_date, department, access_level)
+	INSERT INTO managers (user_id, name,  access_level)
 	values
-	('3','Bruce Wayne','31.12',NOW(),'Front End','3');
+	('3','Bruce Wayne','3');
 	
 	select * from managers;
+	
+	--a Query for all users and their name, Username, and email
+	
+	--a query for all employee for their name, wage and department
+	
+	--a query for the manager so they could see employee name, hire date, wage, schedule,(schedule is optional)
+	
+	--
 	
