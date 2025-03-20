f89d7f9fe7e84694f4d80b21cfa964c0049ccc46
gavin <gavinhosford@gmail.com>
2025-03-20 08:17:15 -0300

    changed lib and added bin

---
+++ GameStoreManagement/src/DatabaseConnection.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/game_store_db";
+    private static final String USER = "root";  
+    private static final String PASSWORD = "test"; 
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println(" MySQL JDBC Driver Loaded Successfully!");
+        } catch (ClassNotFoundException e) {
+            System.err.println(" MySQL JDBC Driver not found! Did you add mysql-connector-java.jar?");
+            e.printStackTrace();
+        }
+
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        System.out.println(" Connected to: " + connection.getCatalog());
+        return connection;
+    }
+}
+
---
+++ GameStoreManagement/src/employee/Employee.java
@@ -0,0 +1,14 @@
+package employee;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author zizoz
+ */
+public class Employee {
+    
+}
