83390db71013540d798d3f48ce7c5d7573378a93
Aakash <aakash.yogabalu@gmail.com>
2025-03-21 16:51:41 -0300

    Updated package.json and vite.config to run in github pages

--- Vite-Frontend/vite.config.js
+++ Vite-Frontend/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/CS2043_GroupProject/Vite-Frontend",
 })
