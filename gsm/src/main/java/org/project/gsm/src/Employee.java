b6bf890b183cc55ed831f77aee19cda68762d6b3
Zaied Ibrahim <154496336+zIAD1507@users.noreply.github.com>
2025-03-25 21:54:06 -0300

    Updated Employee.java with processTransactions method done

--- gsm/src/main/java/org/project/gsm/src/Employee.java
+++ gsm/src/main/java/org/project/gsm/src/Employee.java
@@ -8,7 +8,23 @@ public class Employee extends Person {
 	}
 
 	public void processTransactions(Transaction... transactions) {
-		// TODO: Finish it
-	}
+		  String sql = "INSERT INTO transactions (transaction_id, item_id, date_of_purchase, total) VALUES (?, ?, ?, ?)";
 
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            for (Transaction trans : transactions) {
+                for (int itemId : trans.getItemIDs()) {
+                    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                        stmt.setInt(1, trans.getT_ID());
+                        stmt.setInt(2, itemId);
+                        stmt.setTimestamp(3, new Timestamp(trans.getDateOfPurchase().getTime()));
+                        stmt.setDouble(4, trans.getTotal());
+                        stmt.executeUpdate();
+                        System.out.println("Transaction made for item ID: " + itemId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Cannot process the transactions: " + e.getMessage());
+        }
+    }
 }
